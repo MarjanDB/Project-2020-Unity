cee8fc2fede0836e391d81b6df516986a90c0297
MarjanDB <mario.mocnik@student.um.si>
2021-01-18 22:48:36 +0100

    Data pulling

---
+++ Assets/GlobalSigns.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GlobalSigns : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Signs
+    {
+        public double latitude;
+        public double longitude;
+        public string type;
+    }
+
+
+    [System.Serializable]
+    public class SignWrapper
+    {
+        public Signs[] data;
+    }
+
+
+    public List<Signs> signs = new List<Signs>();
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(makeRequest("http://192.168.1.10:3000/api/getLocations"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    IEnumerator makeRequest(string uri)
+    {
+        using (UnityWebRequest req = UnityWebRequest.Get(uri))
+        {
+            yield return req.SendWebRequest();
+
+
+            if(req.isNetworkError)
+            {
+                Debug.Log(req.error);
+            } else
+            {
+                string res = req.downloadHandler.text;
+                Debug.Log(res);
+                Signs[] l = JsonUtility.FromJson<SignWrapper>(res).data;
+                signs.AddRange(l);
+            }
+
+
+        }
+    }
+}
