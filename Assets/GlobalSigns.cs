a5ed04e43d56d05b39530ca0265768732b48f0ec
MesnaStruca <robert.cvecek@student.um.si>
2021-01-19 21:54:01 +0100

    Added into splash screen and tried to place sign in real world

--- Assets/GlobalSigns.cs
+++ Assets/GlobalSigns.cs
@@ -5,7 +5,6 @@ using UnityEngine.Networking;
 
 public class GlobalSigns : MonoBehaviour
 {
-
     [System.Serializable]
     public class Signs
     {
@@ -29,7 +28,42 @@ public class GlobalSigns : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*
         StartCoroutine(makeRequest("http://192.168.1.10:3000/api/getLocations"));
+        */
+        signs.Add(new Signs { latitude = 46.49339,
+            longitude = 15.6030694, type = "STOP" });
+
+        
+        foreach(Signs x in signs)
+        {
+            GameObject sign = null;
+            bool valid = true;
+            string resource = "";
+            switch (x.type)
+            {
+                case "stop":
+                    resource = ("Signs/STOP");
+                    break;
+                case "prednostna":
+                    resource = ("Signs/PREDNOSTNA");
+                    break;
+                case "odvzem_prednostne":
+                    resource = ("Signs/ODVZEM");
+                    break;
+                default:
+                    valid = false;
+                    break;
+            }
+
+
+            if (valid)
+            {
+                Vector3 pos = new Vector3((float)x.longitude, (float)x.latitude);
+                sign.transform.position = pos;
+                Instantiate(Resources.Load(resource), pos, Camera.main.transform.rotation);
+            }
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/wait.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class wait : MonoBehaviour
+{
+    public float delay = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(1);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
